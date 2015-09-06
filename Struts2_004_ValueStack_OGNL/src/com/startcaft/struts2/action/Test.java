43a24aabbbaa294773e84e9075952154c1f3ccc6
startcaft <51418996@qq.com>
2015-09-06 21:57:01 +0800

    Struts2中的ValueStack和声明式异常

---
+++ Struts2_004_ValueStack_OGNL/src/com/startcaft/struts2/action/Test.java
@@ -0,0 +1,36 @@
+package com.startcaft.struts2.action;
+
+public class Test {
+	
+	private String productName;
+
+	private String productDesc;
+
+	private double productPrice;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductDesc() {
+		return productDesc;
+	}
+
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	
+}
