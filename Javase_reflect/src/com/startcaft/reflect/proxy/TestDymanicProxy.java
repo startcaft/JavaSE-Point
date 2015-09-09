d7f4a4efb41375b4d27c69154dbf997e04891255
startcaft <51418996@qq.com>
2015-09-09 10:19:49 +0800

    动态代理，InvocationHandler代理处理接口，Proxy类的newProxyInstance创建代理对象静态方法

--- Javase_reflect/src/com/startcaft/reflect/proxy/TestDymanicProxy.java
+++ Javase_reflect/src/com/startcaft/reflect/proxy/TestDymanicProxy.java
@@ -4,65 +4,80 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-interface Subject {
-	void action();
+/*普通的接口*/
+interface TargetInterface {
+	public int targetMethodA(int number);
+
+	public int targetMethodB(int number);
 }
 
-// 被代理类
-class RealSubject implements Subject {
+/* 实现接口的类 */
+class ConcreteClass implements TargetInterface {
 
 	@Override
-	public void action() {
-		System.out.println("我是被代理类，记得要执行我...");
+	public int targetMethodA(int number) {
+		System.out.println("开始调用目标类的方法targetMethodA...");
+		System.out.println("操作-打印数字:" + number);
+		System.out.println("结束调用目标类的方法targetMethodA...");
+		return number;
+	}
+
+	@Override
+	public int targetMethodB(int number) {
+		System.out.println("开始调用目标类的方法targetMethodB...");
+		System.out.println("操作-打印数字:" + number);
+		System.out.println("结束调用目标类的方法targetMethodB...");
+		return number;
 	}
 }
 
-class MyInvocationHandler implements InvocationHandler {
+/* 代理处理器类ProxyHandler,必须实现java.lang.reflect.InvocationHandler接口 */
+class ProxyHandler implements InvocationHandler {
 
-	// 被代理类
-	Object target;
+	private Object target;// 被代理的对象，也就是实现了TargetInterface接口的类的对象
 
-	/**
-	 * 返回代理类对象
-	 * 
-	 * @param target
-	 *            ---被代理类对象
-	 * @return 代理对象
-	 */
-	public Object bindTarget(Object target) {
+	public ProxyHandler(Object target) {
 		this.target = target;
-		return Proxy.newProxyInstance(target.getClass().getClassLoader(),
-				target.getClass().getInterfaces(), this);
-	}
-	
-	/**
-	 * 对被代理类的重写的方法的调用都会转化为调用invoke方法
-	 */
-	@Override
-	public Object invoke(Object arg0, Method arg1, Object[] arg2)
-			throws Throwable {
-		System.out.println("invoke");
-		Object returnObj = arg1.invoke(arg0, arg2);
-		return returnObj;
 	}
 
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		System.out.println("proxy:" + proxy.getClass().getName());
+		System.out.println("method:" + method.getName());
+		for (Object object : args) {
+			System.out.println("method agrs:" + object.getClass().getName());
+		}
+
+		System.out.println("BeFore invoke method...");
+		Object obj = method.invoke(target, args);
+		System.out.println("After invoke method...");
+
+		return obj;
+	}
 }
 
 public class TestDymanicProxy {
 
 	public static void main(String[] args) {
-		//1.创建一个被代理类对象
-		RealSubject real = new RealSubject();
+
+		// 元对象(被代理对象)
+		ConcreteClass target = new ConcreteClass();
+
+		// 代理处理程序
+		InvocationHandler handler = new ProxyHandler(target);
+
+		// 返回指定接口的代理类实例
+		TargetInterface proxyTarget = (TargetInterface) Proxy.newProxyInstance(
+				target.getClass().getClassLoader(), target.getClass()
+						.getInterfaces(), handler);
 		
-		//2.创建一个实现了InvocationHandler接口的对象
-		MyInvocationHandler myInvokeHandler = new MyInvocationHandler();
+		//调用代理类实力的方法，Java会执行InvocationHandler接口中的invoke方法
+		int i = proxyTarget.targetMethodA(5);
+		System.out.println(i);
 		
-		//3.返回代理对象
-		Object obj = myInvokeHandler.bindTarget(real);
-		
-		//4.强制类型转换
-		Subject proxy = (Subject) obj;
-		
-		proxy.action();
+		int j = proxyTarget.targetMethodB(15);
+		System.out.println(j);
+
 	}
 }
